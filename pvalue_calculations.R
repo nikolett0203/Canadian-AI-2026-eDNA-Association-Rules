16a89bd4b8e473186be9cc1c8bb3301df30c52ba
niki <nikolett0203@gmail.com>
2026-02-16 02:44:29 -0500

    beginning rewrite of final, clean script

--- pvalue_calculations.R
+++ pvalue_calculations.R
@@ -1,6 +1,111 @@
 ####### Libraries #######
 
 library(RulesTools)
+
+
+
+
+####### Analysis Parameters #######
+
+NUM_TRANSACTIONS <- 126
+
+covariates <-c(
+  "eDNAConc",
+  "eFishCatch",
+  "AirTemp",
+  "WaterTemp",
+  "pH",
+  "DissolvedOxygen",
+  "Conductivity",
+  "VolumeFiltered"
+)
+
+labels <- c(
+  "eDNA Concentration (Copies/µL)",
+  "Electrofish Catch",
+  "Air Temperature (°C)",
+  "Water Temperature (°C)",
+  "pH",
+  "Dissolved Oxygen (mg/L)",
+  "Conductivity (µS)",
+  "Volume Filtered (mL)"
+)
+
+discretizations <- c(
+  13.3,                      # for eDNA concentrations (50% LOD)
+  0,                         # for electrofish catch (absence/presence)
+  15.03,                     # for air temp (avg temp for sample site in Sept)
+  15,                        # for water temp (optimal temp for brook trout)
+  7.75,                      # for pH (CCME guideline)
+  9.5,                       # for dissolved oxygen (CCME guideline)
+  1047,                      # for conductivity (median)***
+  1.04                       # for volume filtered (median)
+)
+
+consequents <- c(
+  "eDNAConc=high", 
+  "eFishCatch=present" 
+)
+
+
+
+
+####### Data Pre-Processing #######
+
+# init empty df
+discretized_df <- data.frame(matrix(nrow=NUM_TRANSACTIONS, ncol=0))
+
+# add discretized values to df
+for(i in seq_along(discretizations)){
+  
+  if(covariates[i] == "eFishCatch") {
+    temp <- dtize_col(
+      column = BrookTrout[[covariates[i]]],
+      cutoff = discretizations[i],
+      infinity = TRUE,
+      labels = c("absent", "present")
+    )
+  }
+  
+  else {
+    temp <- dtize_col(
+      column = BrookTrout[[covariates[i]]],
+      cutoff = discretizations[i],
+      infinity = TRUE
+    )
+  }
+  
+  discretized_df[[covariates[i]]] <- temp
+  
+}
+
+# backpack and site already discrete, so they can be added to df directly
+discretized_df[["Backpack"]] <- as.factor(BrookTrout$Backpack)
+discretized_df[["Site"]] <- as.factor(BrookTrout$Site)
+
+# convert df into transactions
+transactions <- as(discretized_df, "transactions")
+
+####### Rule Mining #######
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### Libraries #######
+
+
 library(arules)
 library(tidyr)
 library(ggplot2)
