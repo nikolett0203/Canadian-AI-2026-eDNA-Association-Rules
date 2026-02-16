9e2b3de607a248fe6b577cd30f1b5b014b73292b
niki <nikolett0203@gmail.com>
2026-02-16 13:49:28 -0500

    updated pvalue function (runtime down by 99%, 15m -> 12s)

--- pvalue_calculations.R
+++ pvalue_calculations.R
@@ -1,7 +1,7 @@
 ####### Libraries #######
 
 library(RulesTools)
-
+library(arules)
 
 
 
@@ -50,6 +50,53 @@ consequents <- c(
 
 
 
+####### Helper Functions #######
+
+get_p <- function(rules, transactions) {
+  
+  quality_df <- quality(rules)
+  n <- length(transactions)                       # could also use NUM_TRANSACTIONS but this is more general
+  
+  lhs <- unique(lhs(rules))
+  rhs <- unique(rhs(rules))
+  
+  lhs_supp <- support(lhs, transactions) * n      # support() returns fraction, multiply by n to get count
+  rhs_supp <- support(rhs, transactions) * n
+  
+  # create support lookup table
+  lhs_labels <- labels(lhs)
+  rhs_labels <- labels(rhs)
+  names(lhs_supp) <- lhs_labels
+  names(rhs_supp) <- rhs_labels
+  
+  # vectorized p-value calcs
+  pvalues <- vapply(1:length(rules), function(i) {
+    
+    # get count of antecedent&consequent
+    a <- quality_df$count[i]
+    
+    # lookup count of antecedent
+    lhs_label <- labels(lhs(rules[i]))
+    b <- lhs_supp[lhs_label] - a
+    
+    # lookup count of consequent
+    rhs_label <- labels(rhs(rules[i]))
+    c <- rhs_supp[rhs_label] - a
+    
+    d <- n - a - b - c
+    
+    fisher.test(matrix(c(a, b, c, d), nrow = 2), 
+                alternative = "greater")$p.value
+    
+  }, FUN.VALUE = numeric(1))
+  
+  return(pvalues)
+
+}
+
+
+
+
 ####### Data Pre-Processing #######
 
 # init empty df
@@ -86,6 +133,9 @@ discretized_df[["Site"]] <- as.factor(BrookTrout$Site)
 # convert df into transactions
 transactions <- as(discretized_df, "transactions")
 
+
+
+
 ####### Rule Mining #######
 
 
@@ -106,7 +156,7 @@ transactions <- as(discretized_df, "transactions")
 ####### Libraries #######
 
 
-library(arules)
+
 library(tidyr)
 library(ggplot2)
 library(viridis)
@@ -119,45 +169,7 @@ library(dplyr)
 
 ####### P-Value Function #######
 
-# fix for edge cases, non-integer contingency cells, etc
-get_pvalues <- function(rules, transactions) {
-  
-  # stores support, confidence, lift, count, etc. because we want count
-  quality_df <- quality(rules)
-  
-  n <- length(transactions)
-  
-  p_values <- sapply(1:length(rules), function(i) {
-    
-    rule <- rules[i]
-    
-    lhs_items <- lhs(rule)
-    rhs_items <- rhs(rule)
-    
-    # figure out how many transactions have rhs and lhs together
-    a <- quality_df$count[i]
-    
-    # figure out how many had lhs only 
-    # support returns fraction of itemsets with rule so we *n to get raw count
-    lhs_count <- support(lhs_items, transactions) * n
-    b <- lhs_count - a
-    
-    # rhs only
-    rhs_count <- support(rhs_items, transactions) * n
-    c <- rhs_count - a
-    
-    # count of transactions with neither lhs nor rhs
-    d <- n - a - b - c
-    
-    # build contingency table 
-    cont_table <- matrix(c(a, b, c, d), nrow = 2)
-    
-    # do the test
-    fisher.test(cont_table, alternative = "greater")$p.value
-  })
-  
-  return(p_values)
-}
+
 
 
 
@@ -281,101 +293,6 @@ spearman_table <- function(df) {
       p_BF = p.adjust(praw, method = "bonferroni")
     )
 }
-
-
-
-
-
-####### Analysis Parameters #######
-
-NUM_TRANSACTIONS <- 126
-
-covariates <-c(
-  "eDNAConc",
-  "eFishCatch",
-  "AirTemp",
-  "WaterTemp",
-  "pH",
-  "DissolvedOxygen",
-  "Conductivity",
-  "VolumeFiltered"
-)
-
-labels <- c(
-  "eDNA Concentration (Copies/µL)",
-  "Electrofish Catch",
-  "Air Temperature (°C)",
-  "Water Temperature (°C)",
-  "pH",
-  "Dissolved Oxygen (mg/L)",
-  "Conductivity (µS)",
-  "Volume Filtered (mL)"
-)
-
-discretizations <- c(
-  13.3,                      # for eDNA concentrations (50% LOD)
-  0,                         # for electrofish catch (absence/presence)
-  15.03,                     # for air temp (avg temp for sample site in Sept)
-  15,                        # for water temp (optimal temp for brook trout)
-  7.75,                      # for pH (CCME guideline)
-  9.5,                       # for dissolved oxygen (CCME guideline)
-  1047,                      # for conductivity (median)***
-  1.04                       # for volume filtered (median)
-)
-
-consequents <- c(
-  "eDNAConc=high", 
-  #"eDNAConc=low", 
-  "eFishCatch=present" 
-  #"eFishCatch=absent"
-)
-
-plot_titles <- list(
-  "{eDNAConc=high}",
-  #"{eDNAConc=low}",
-  "{eFishCatch=present}"
-  #"{eFishCatch=absent}"
-)
-
-
-
-
-
-####### Data Pre-Processing #######
-
-# init empty df
-discretized_df <- data.frame(matrix(nrow=NUM_TRANSACTIONS, ncol=0))
-
-# add discretized values to df
-for(i in seq_along(discretizations)){
-  
-  if(covariates[i] == "eFishCatch") {
-    temp <- dtize_col(
-      column = BrookTrout[[covariates[i]]],
-      cutoff = discretizations[i],
-      infinity = TRUE,
-      labels = c("absent", "present")
-    )
-  }
-  
-  else {
-    temp <- dtize_col(
-      column = BrookTrout[[covariates[i]]],
-      cutoff = discretizations[i],
-      infinity = TRUE
-    )
-  }
-  
-  discretized_df[[covariates[i]]] <- temp
-  
-}
-
-# backpack and site already discrete, so they can be added to df directly
-discretized_df[["Backpack"]] <- as.factor(BrookTrout$Backpack)
-discretized_df[["Site"]] <- as.factor(BrookTrout$Site)
-
-# convert df into transactions
-transactions <- as(discretized_df, "transactions")
 
 
 
